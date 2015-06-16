7d5e865f0fdb994e9b69d61fd717be8da1737bd9
jay-home <jh@talentedsquare.com>
2015-06-15 18:38:55 -0700

    PerfLogger integration has been added to asda_to_local.sh

--- asda_to_local.sh
+++ asda_to_local.sh
@@ -9,12 +9,15 @@ then
 fi
 
 cd $1
+
 # Configuring for local usage
 sed -i 's/useMinifiedFiles" value="true/useMinifiedFiles" value="false/' wwwroot/htmlTemplates/pages/common/common-config.shtml
 sed -i 's/useImageServer" value="true/useImageServer" value="false/' wwwroot/htmlTemplates/pages/common/common-config.shtml
 sed -i 's/cssBasePath" value="${HTTP_cssBasePath/cssBasePath" value="\/\/${HTTP_HOST/' wwwroot/htmlTemplates/pages/common/common-config.shtml
+
 # Fixing a small bug
 sed -i 's/allContentStyle_1506/allContentStyle_1504/' wwwroot/htmlTemplates/components/document_head.shtml
+
 #Get responsive CSS files to local
 mkdir wwwroot/assets/theme_default/style/min
 mkdir wwwroot/assets/theme_default/style/min/responsive
@@ -22,3 +25,10 @@ cd wwwroot/assets/theme_default/style/min/responsive
 wget http://ui1.assets-asda.com/theme/style/min/responsive/signin.css
 wget http://ui1.assets-asda.com/theme/style/min/responsive/responsive.css
 wget http://ui1.assets-asda.com/theme/style/min/responsive/responsiveGlobal.css
+
+# Integrating PerfLogger library to local web repository
+cd $1/wwwroot/assets/lib/
+wget --no-check-certificate https://raw.githubusercontent.com/tomjbarker/perfLogger/master/perfLogger.js
+sed -i '25i\
+\t\t<script src="<!--#echo var="jsBasePath"-->\/lib\/perfLogger.js"><\/script>\r\
+\t\t<style>#debug {display: block!important;}<\/style>\r' $1/wwwroot/htmlTemplates/components/body_footer.shtml
