3e2517364c72efd94ebe1d2ed6417bdea619d826
jay-home <jh@talentedsquare.com>
2015-06-24 05:47:02 -0700

    Visual improvements on coverage.js

--- js/coverage.js
+++ js/coverage.js
@@ -4,21 +4,37 @@
 var coverage = function(){
 
 	function drawToScreen(fileName, funcName){
-		var cDiv = document.getElementById("coverage_div");
+		var cDiv = document.getElementById("coverageDiv");
+		var hDiv = document.getElementById("coverageHeader");
 		var fileID = fileName.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
 		var funcID = funcName.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
 		
 		if(!cDiv){
 			cDiv = document.createElement("div");
-			cDiv.id = "coverage_div";
+			cDiv.id = "coverageDiv";
 			$(cDiv).css({  'width' : '500px', 'position' : 'absolute', 
 						   'top' : '0px', 'right' : '0px', 'background-color' : 'rgb(158, 225, 74)', 
 						   'z-index' : '1000', 'opacity' : '0.93', 'padding' : '7px'
 						});
 			document.body.appendChild(cDiv);
-			document.body.appendChild(document.createElement("hr")); 		  
+			hDiv=document.createElement("div");
+			hDiv.id = "coverageHeader";
+			$(hDiv).css({ 'color': '#7c795d',
+						  'font-family' : 'Trocchi, Serif',
+						  'font-size': '25px',
+						  'font-weight' : 'normal',
+						  'line-height' : '48px',
+						  'margin': '0',
+						  'position': 'absolute',
+						  'right': '-80px',
+						  'transform': 'rotate(270deg)',
+						  'top': '120px',
+						  'transform-origin': 'left top 0'
+						});						
+			hDiv.innerHTML="CoverageJS";
+			cDiv.appendChild(hDiv);			
 		}
-
+		
 		if ($(cDiv).find( '#' + fileID).length == 0) {
 			var fileDiv = document.createElement("p");
 			fileDiv.id = fileID;
@@ -26,12 +42,19 @@ var coverage = function(){
 			$(cDiv).append(fileDiv);
 		}
 		
-		if ($(cDiv).find( '#' + fileID + ' #' + funcID).length == 0) {
-			 $(cDiv).find( '#' + fileID).append("<div id='" + funcID + "'>" + funcName + ": <span id='count'>1</span> times.</div>");			 
+		funcDiv=$(cDiv).find( '#' + fileID + ' #' + funcID);
+		if ($(funcDiv).length == 0) {
+			 $(cDiv).find( '#' + fileID).append("<div id='" + funcID + "' class='coverageFunction'>" + funcName + ": <span id='count'>1</span> times.</div>");			 
 		}
 		else {
-			var cSpan = $(cDiv).find( '#' + fileID + ' #' + funcID + ' #count');
+			var cSpan = $(funcDiv).find('#count');
 			cSpan.text(parseInt(cSpan.text())+1);
+			$(funcDiv).css({'color' : 'DarkRed', 'background-color' : 'rgb(48, 223, 206)'});
+			$(hDiv).css('color' , 'DarkRed');
+			setTimeout(function(){
+							$(cDiv).find('.coverageFunction').css({'color' : 'Black', 'background' : 'none'});
+							$(hDiv).css('color' , '#7c795d');
+					   }, 2000);
 		}
 	}
 
